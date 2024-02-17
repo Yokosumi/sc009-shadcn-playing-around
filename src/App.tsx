680d6e7fd7d8638c848f4a7bce2f6d3c91c8da19
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-02-17 19:32:23 +0100

    Update header in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
     return (
         <>
             <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-                Header 1
+                Header
             </h1>
         </>
     )
