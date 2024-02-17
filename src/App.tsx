c9f774f57d46f8e71e706aed0eb35675c04c8b17
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-02-17 20:02:24 +0100

    Update App component and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 function App() {
     return (
         <>
-            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-                Header
-            </h1>
+            <h1>hello</h1>
         </>
     )
 }
